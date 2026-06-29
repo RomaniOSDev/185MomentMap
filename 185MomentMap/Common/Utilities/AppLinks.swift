990a344c40bf632bd892def34d86ac38ce569efc
Roman <romazaniosdev@gmail.com>
2026-06-29 14:38:26 +0300

    final

--- 185MomentMap/Common/Utilities/AppLinks.swift
+++ 185MomentMap/Common/Utilities/AppLinks.swift
@@ -7,9 +7,9 @@ enum AppLinks {
     var urlString: String {
         switch self {
         case .privacyPolicy:
-            return "https://example.com/privacy-policy"
+            return "https://www.termsfeed.com/live/f48d523d-0c00-48d0-ac6e-3a9f1566b882"
         case .termsOfUse:
-            return "https://example.com/terms-of-use"
+            return "https://www.termsfeed.com/live/016a3bf9-08e5-4d66-bae9-6968272294b9"
         }
     }
 
